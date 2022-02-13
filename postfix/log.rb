6f83eed6b453812ab1f6b805a3b49f2fa4e7be38
netebakari <itsumo.itsumo.nemui@gmail.com>
2022-02-12 22:59:22 -0800

    コンテナをUbuntu 20.10から20.04に変更

--- postfix/log.rb
+++ postfix/log.rb
@@ -18,7 +18,20 @@ FileUtils.chmod(0666, "/mailraw/#{date}/#{timestamp}.txt")
 raw.force_encoding("utf-8")
 mail = Mail.new(raw)
 
+def join_something(str)
+  return "-" if str == nil
+  return str if str.is_a?(String)
+  return str.join(",") if str.is_a?(Array)
+  return str.to_s
+end
+
+to      = join_something(mail.to)
+from    = join_something(mail.from)
+cc      = join_something(mail.cc)
+subject = mail.subject.gsub("\t", " ")
+
 open("/maillist/#{date}.csv", "a"){|f|
-  f.puts [DateTime.now.to_s, mail.to, mail.from, mail.cc, mail.subject, mail.sender].join("\t")
+  f.puts [DateTime.now.to_s, to, from, cc, subject, mail.sender].join("\t")
 }
+
 FileUtils.chmod(0666, "/maillist/#{date}.csv")
